8ca6d6fb54b1e602279fc0534a991fd5c3a5489e
Ayush-Bulbule <ayushbulbule24@gmail.com>
2024-05-03 13:25:33 +0530

    Added floting menu to prospects

--- App.js
+++ App.js
@@ -2,15 +2,19 @@ import AppNavigation from "./navigation/appNavigation";
 import * as eva from "@eva-design/eva";
 import { NavigationContainer } from "@react-navigation/native";
 import { ApplicationProvider } from "@ui-kitten/components";
+import { MenuProvider } from "react-native-popup-menu";
+
 import { UserRoleProvider } from "./context/UserContext";
 export default function App() {
   return (
     <ApplicationProvider {...eva} theme={eva.light}>
-      <UserRoleProvider>
-        <NavigationContainer>
-          <AppNavigation />
-        </NavigationContainer>
-      </UserRoleProvider>
+      <MenuProvider>
+        <UserRoleProvider>
+          <NavigationContainer>
+            <AppNavigation />
+          </NavigationContainer>
+        </UserRoleProvider>
+      </MenuProvider>
     </ApplicationProvider>
   );
 }
--- components/Dashboard/ProspectUserListItem.js
+++ components/Dashboard/ProspectUserListItem.js
@@ -13,12 +13,27 @@ import React, {
   useMemo,
   useRef,
 } from "react";
+
+// somewhere in your app
+import {
+  Menu,
+  MenuOptions,
+  MenuOption,
+  MenuTrigger,
+} from "react-native-popup-menu";
+
 import Avatar02 from "../../assets/icons/avatar_02.svg";
 import { Swipeable } from "react-native-gesture-handler";
 import IcCloseX from "../../assets/icons/ic_close_x.svg";
 import BgCorner from "../../assets/images/prospect_corner_bg.png";
 import IcMore from "../../assets/icons/ic_vertical_dots.svg";
 import Checkbox from "../ui/Checkbox";
+import MoreCall from "../../assets/icons/more_call.png";
+import MoreEmail from "../../assets/icons/more_mail.png";
+import MoreWhatsapp from "../../assets/icons/more_whatsapp.png";
+import IcMorePhone from "../../assets/icons/more_phone.svg";
+import IcMoreEmail from "../../assets/icons/more_email.svg";
+import IcMoreWhatsapp from "../../assets/icons/more_whatsapp.svg";
 
 const LeftSwipeActions = () => {
   return (
@@ -64,16 +79,7 @@ const ProspectUserListItem = ({
       onSwipeableLeftOpen={swipeFromLeftOpen}
       onSwipeableRightOpen={swipeFromRightOpen}
     >
-      <View className=" bg-white  shadow-sm m-2 border-gray-300 shadow-gray-500 rounded-xl ">
-        <TouchableWithoutFeedback onPress={handleLeftSwipe}>
-          <ImageBackground
-            source={BgCorner}
-            borderTopRightRadius={10}
-            className="w-10 h-16 absolute top-0 right-0 rounded-tr-xl flex items-center pt-4"
-          >
-            <IcMore />
-          </ImageBackground>
-        </TouchableWithoutFeedback>
+      <View className=" bg-white  shadow-sm m-2 border-gray-300 shadow-gray-500 rounded-xl flex-row justify-between">
         <View className="py-6 rounded-lg p-2 flex-row items-center">
           {bulk ? <Checkbox /> : null}
           <Avatar02 width="64" height="64" />
@@ -82,6 +88,86 @@ const ProspectUserListItem = ({
             <Text className="text-sm text-gray-400">{item.email}</Text>
           </View>
         </View>
+
+        {/* More actions */}
+        <ImageBackground
+          source={BgCorner}
+          borderTopRightRadius={10}
+          className="w-10 h-16 rounded-tr-xl flex items-center "
+        >
+          <Menu>
+            <MenuTrigger
+              style={{
+                width: 35,
+                height: "85%",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                borderTopRightRadius: 15,
+              }}
+            >
+              <IcMore />
+            </MenuTrigger>
+            <MenuOptions
+              customStyles={{
+                optionsContainer: {
+                  marginTop: 60,
+                  alignItems: "flex-end",
+                  backgroundColor: "transparent",
+                  shadowColor: "transparent",
+                  display: "flex",
+                  alignContent:"center"
+                },
+                optionWrapper: {
+                  backgroundColor: "transparent",
+                  shadowColor: "transparent",
+                },
+              }}
+            >
+              <MenuOption
+                customStyles={{
+                  optionWrapper: {
+                    backgroundColor: "transparent",
+                  },
+                }}
+                onSelect={() => alert(`Contact via Phone`)}
+              >
+                <View className="w-10 h-10 rounded-full shadow-2xl  shadow-gray-900 bg-amber-50 flex items-center justify-center">
+                  <IcMorePhone width={20} height={20}/>
+                </View>
+              </MenuOption>
+
+
+              <MenuOption
+                customStyles={{
+                  optionWrapper: {
+                    backgroundColor: "transparent",
+                  },
+                }}
+                onSelect={() => alert(`Contact via Phone`)}
+              >
+                <View className="w-10 h-10 rounded-full shadow-2xl  shadow-gray-900 bg-amber-50 flex items-center justify-center">
+                  <IcMoreEmail width={20} height={20}/>
+                </View>
+              </MenuOption>
+
+              <MenuOption
+                customStyles={{
+                  optionWrapper: {
+                    backgroundColor: "transparent",
+                  },
+                }}
+                onSelect={() => alert(`Contact via Phone`)}
+              >
+                <View className="w-10 h-10 rounded-full shadow-2xl  shadow-gray-900 bg-amber-50 flex items-center justify-center">
+                  <IcMoreWhatsapp width={20} height={20}/>
+                </View>
+              </MenuOption>
+
+
+            </MenuOptions>
+          </Menu>
+        </ImageBackground>
       </View>
     </Swipeable>
   );
